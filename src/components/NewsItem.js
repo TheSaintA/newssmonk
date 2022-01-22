ff94499f25ce8413da0204d46ccdec958ceae182
The Saint <wanizakir073@gmail.com>
2022-01-22 11:31:23 +0530

    Minor errors refactored

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
         </span>
             <img style={{maxHeight:"150px"}} src={imageUrl?imageUrl:"https://thumbs.dreamstime.com/z/world-news-11655845.jpg"} className="card-img-top" alt="..."/>
             <div className="card-body px-0">
-                <p className="card-title  fw-bold rounded p-1 mx-0 bg-dark">{title}</p>
+                <p className="card-title  fw-bold rounded p-1 mx-0 ">{title}</p>
                 <p className="card-text text-wrap shadow-lg rounded p-2">{description}</p>
                 <p className="card-text mx-2 fs-6">By {author?author:"Uknown"} on {new Date(publishedAt).toGMTString()}</p>
                 <a rel="norefferrer" href={link} target="_blank" className="btn btn-dark mx-2">Read more...</a>
